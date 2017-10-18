b49c5b97ae1621010b7ab33bf4f7d9be8fe4785b
rajukoushik <g.rajukoushik@gmail.com>
2017-10-18 14:54:25 +0530

    Added 'blog' app.
    Signed off by - Raju Koushik g.rajukoushik@gmail.com

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^(?P<post_id>[0-9]+)/$', views.indi_post, name='indi_post'),
+    url(r'^about/', views.about, name='about'),
+
+
+]
